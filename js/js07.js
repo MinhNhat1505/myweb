e8988e5890cb4dc6d519621067ee7228bfbd8543
MinhNhat1505 <nhatthptbhh@gmail.com>
2022-08-21 00:24:44 +0700

    up

--- js/js07.js
+++ js/js07.js
@@ -1,83 +1,33 @@
-function reverse() {
-    var s = document.getElementById("s1").value;
-    arr = s.split(" ");//tách chuỗi vào mảng
-    var i,kq = "";
-    for(i = arr.length - 1;i >= 0;i--)
-        kq += arr[i].trim() + " ";
-    document.getElementById("s1").value = kq.trim();
-}
+function reset() {
+    document.getElementById("s1").value = strOrigin;
+ }
 
-function normal() {
-    var s = document.getElementById("s1").value;
-    arr = s.split(" ");
-    var i,kq= "",tmp;
-    for(i = 0;i < arr.length;i++)
+ function normal() {
+    var s1 = document.getElementById("s1").value;
+    arr = s1.split(" ");//tách chuỗi vào mảng
+    var kq = "";
+    for(let i = 0;i < arr.length;i++)
     {
-        tmp = arr[i].trim().toLowerCase();
-        if(tmp != "")
-        {
-            //in hoa ký tự đầu
+        let tmp = arr[i].trim().toLowerCase(); // lấy từng phần tử cho về in thường
+        if(tmp != "") {
             tmp = tmp.substr(0,1).toUpperCase() + tmp.substr(1);
             kq += tmp + " ";
         }
     }
     document.getElementById("s1").value = kq.trim();
-    console.log(strOrigin);
-}
+ }
 
-function reset() {
-    console.log(strOrigin);
-    document.getElementById("s1").value = strOrigin;
-}
+ function reverse() {
+    var s1 = document.getElementById("s1").value;
+    arr = s1.split(" ");
+    var kq = "";
+    for(let i = arr.length - 1;i >= 0;i--)
+        kq += arr[i].trim() + " ";
+    document.getElementById("s1").value = kq.trim();
+ }
 
-function replaceAll() {
+ function replaceAll() {
     var s1 = document.getElementById("s1").value;
     var s2 = document.getElementById("s2").value;
     var s3 = document.getElementById("s3").value;
-    var token = new RegExp(s2,"ig");
-    s1 = s1.replace(token,s3);//thay thế s2 = s3 ko phân biệt in hoa
-    document.getElementById("s1") = s1;
-}
-
-function delAll() {
-    var s1 = document.getElementById("s1").value;
-    var s2 = document.getElementById("s2").value;
-    var token = new RegExp(s2,"ig");
-    s1 = s1.replace(token,'');//thay thế s2 = s3 ko phân biệt in hoa
-    document.getElementById("s1") = s1;
-}
-
-function insert() {
-    var s1 = document.getElementById("s1").value;
-    var s2 = document.getElementById("s2").value;
-    var pos = document.getElementById("txtPos").value;
-    if(pos == "")
-    {
-        alert("Vị trí chưa nhập");
-        return false;
-    }
-    pos = Number(pos);
-    if(isNaN(pos) || pos < 0 || pos > s1.length)
-    {
-        alert("Vị trí không hợp lệ");
-        return false;
-    }
-    if(s2 == "")
-    {
-        alert("Chưa nhập chuỗi s2");
-        return false;
-    }
-    var kq = insertAt(s1,s2,pos);
-    if(kq)
-        document.getElementById("s1").value = kq;
-}
-
-function insertAt(main_string,int_string,pos) {
-    return main_string.slice(0,pos) + int_string + main_string.slice(pos);
-}
-
-function xoayChu() {
-    var str = document.getElementById("title").value;
-    str = str.substr(str.length - 1) + str.substr(0,str.length - 1);
-    document.getElementById("title").innerHTML = str;
-}
+ }
